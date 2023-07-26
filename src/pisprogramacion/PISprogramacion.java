dce262097d0015b01f8bc5ad2a01c275184aa95c
santi <santi@LENOVO>
2023-07-26 09:13:04 -0500

    hola

---
+++ src/pisprogramacion/PISprogramacion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pisprogramacion;
+
+/**
+ *
+ * @author santi
+ */
+public class PISprogramacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
