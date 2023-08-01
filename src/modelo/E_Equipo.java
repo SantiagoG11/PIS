e3e438bb5f9768384d48a5db8b514af57abef44f
Usuario iTC <Usuario iTC@LAPTOP-FVG0G8PJ>
2023-08-01 08:24:09 -0500

    Revert "Revert "Actualizacion 1.1 Modelo Final""
    This reverts commit 1526e1faa6648f42564c05159dd360a8652a80d2.

---
+++ src/modelo/E_Equipo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author santi
+ */
+public class E_Equipo {
+    private int partidosGanados;
+    private int partidosEmpatados;
+    private int partidosPerdidos;
+    private Equipo equipo;
+}
