3f1f047cf50cdec0732761af70ef472679654ba6
Shifu8 <141149537+Shifu8@users.noreply.github.com>
2023-08-15 10:48:01 -0500

    actualizacao

--- src/vista/EquipoAbstractModel.java
+++ src/vista/EquipoAbstractModel.java
@@ -20,34 +20,65 @@ public class EquipoAbstractModel extends AbstractTableModel{
 public EquipoAbstractModel(List<Equipo> listaEquipos){
         super();
         this.listaEquipos = listaEquipos;
-        this.titleColumns = new String[]{"Nombres", "Apellidos", "Cedula", "Nacionalidad", "Edad", "Dorsal", "Posicion"};
+        this.titleColumns = new String[]{"Nombre", "Ciudad", "Entrenador"};
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ public void actualizar(List<Equipo> listaEquipos) {
+     this.setListaEquipos(listaEquipos);
+        fireTableDataChanged();
+    }
     
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return getListaEquipos().size();
+
     }
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return 3;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Equipo equipo = getListaEquipos().get(rowIndex);
+        switch(columnIndex){
+            case 0:
+                return equipo.getNombre();
+            case 1:
+                return equipo.getCiudad();
+            case 2:
+                return equipo.getEntrenador(); 
+        }
+        return null;
+    }
+
+    /**
+     * @return the titleColumns
+     */
+    public String[] getTitleColumns() {
+        return titleColumns;
+    }
+
+    /**
+     * @param titleColumns the titleColumns to set
+     */
+    public void setTitleColumns(String[] titleColumns) {
+        this.titleColumns = titleColumns;
+    }
+
+    /**
+     * @return the listaEquipos
+     */
+    public List<Equipo> getListaEquipos() {
+        return listaEquipos;
+    }
+
+    /**
+     * @param listaEquipos the listaEquipos to set
+     */
+    public void setListaEquipos(List<Equipo> listaEquipos) {
+        this.listaEquipos = listaEquipos;
     }
     
 }
