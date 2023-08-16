2b833340622932fcc8fc947482298dcab99128bc
Santiago2k4 <ordonezsantiago2k4@gmail.com>
2023-08-16 17:28:41 -0500

    actu general

--- src/vista/FrmGeneral.java
+++ src/vista/FrmGeneral.java
@@ -36,42 +36,76 @@ public class FrmGeneral extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel26 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
         jLabel10 = new javax.swing.JLabel();
         pl = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel26.setBackground(new java.awt.Color(255, 102, 102));
+        jPanel26.setBackground(new java.awt.Color(0, 0, 0));
         jPanel26.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel10.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel10.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
-        jLabel10.setText("GENERAL");
-        jPanel26.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 50, 110, 20));
-
-        pl.setBackground(new java.awt.Color(255, 255, 255));
-
-        javax.swing.GroupLayout plLayout = new javax.swing.GroupLayout(pl);
-        pl.setLayout(plLayout);
-        plLayout.setHorizontalGroup(
-            plLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 660, Short.MAX_VALUE)
-        );
-        plLayout.setVerticalGroup(
-            plLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 380, Short.MAX_VALUE)
-        );
-
-        jPanel26.add(pl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 130, 660, 380));
-
+        jButton1.setBackground(new java.awt.Color(204, 204, 204));
+        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 51, 51));
         jButton1.setText("DETALLES DEL PARTIDO");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel26.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 50, -1, -1));
+        jPanel26.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 150, -1, -1));
+
+        jLabel10.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel10.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel10.setText("GENERAL");
+        jPanel26.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 120, 110, 20));
+
+        pl.setBackground(new java.awt.Color(204, 204, 204));
+        pl.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setText("Imagen equipo 1");
+        jLabel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        pl.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 90, 180, 150));
+
+        jLabel3.setText("Imagen equipo 2");
+        jLabel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        pl.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 90, 180, 150));
+
+        jLabel2.setText("Estadio");
+        pl.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 250, -1));
+
+        jLabel4.setText("Fecha");
+        pl.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 40, 80, -1));
+
+        jLabel5.setText("Estado del partido");
+        pl.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 10, 120, -1));
+
+        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/flechaiz.png"))); // NOI18N
+        pl.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 140, 40, -1));
+
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/flechad.png"))); // NOI18N
+        pl.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 150, 40, -1));
+
+        jPanel26.add(pl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 190, 660, 330));
+
+        jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Mi proyecto (2) (1).png"))); // NOI18N
+        jPanel26.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 0, -1, -1));
+
+        jLabel11.setFont(new java.awt.Font("ROG Fonts", 1, 18)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(255, 102, 102));
+        jLabel11.setText("F-natics");
+        jPanel26.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 100, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -133,7 +167,16 @@ public class FrmGeneral extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel26;
     private javax.swing.JPanel pl;
     // End of variables declaration//GEN-END:variables
